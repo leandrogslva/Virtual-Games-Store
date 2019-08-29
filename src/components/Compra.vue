38934b0c6e1b6e5b578c47160fbe38490ffca045
Leandro <leandrogslva@gmail.com>
2019-08-29 15:27:07 -0300

    feat: iniciando componente de compra dos jogos

---
+++ src/components/Compra.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-container>
+            <v-layout>
+                <v-flex>
+
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>    
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+
+    methods: {
+       
+    }
+}
+</script>
+<style>
+
+</style>
